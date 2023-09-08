1390fbe64ae6e4436cc70b4e7c70f57496a35b31
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-09 00:59:33 +0500

    added controllers & repo

--- src/main/java/ru/wms/WarehouseManagementService/entity/Order.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/Order.java
@@ -1,8 +1,6 @@
 package ru.wms.WarehouseManagementService.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +20,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "creation_date")
+    @Column(name = "creation_date", columnDefinition = "DATE")
     private LocalDate creationDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
@@ -31,61 +29,4 @@ public class Order {
     @Column(name = "total_cost")
     private BigDecimal totalCost;
     private boolean delivery;
-
-    // getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(BigDecimal totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public boolean isDelivery() {
-        return delivery;
-    }
-
-    public void setDelivery(boolean delivery) {
-        this.delivery = delivery;
-    }
 }
