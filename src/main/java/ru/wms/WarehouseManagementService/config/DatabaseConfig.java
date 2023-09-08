0bf1995f3c4ff2049b87505266e85b4aae7d137a
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-08 23:01:48 +0500

    added DatabaseConfig

---
+++ src/main/java/ru/wms/WarehouseManagementService/config/DatabaseConfig.java
@@ -0,0 +1,53 @@
+package ru.wms.WarehouseManagementService.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("ru.wms.WarehouseManagementService")
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/warehouse");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+
+        return dataSource;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+        factoryBean.setPackagesToScan("ru.wms.WarehouseManagementService");
+        factoryBean.setDataSource(dataSource());
+        factoryBean.afterPropertiesSet();
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory());
+
+        return transactionManager;
+    }
+}
