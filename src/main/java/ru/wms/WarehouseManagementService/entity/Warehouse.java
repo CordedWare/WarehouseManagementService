090c15ed7c6be51d3166a4f5680fd783378bdc54
ArtyomDulya <111312308+ArtyomDulya@users.noreply.github.com>
2023-09-09 00:46:24 +0500

    fix Warehouse (#4)

--- src/main/java/ru/wms/WarehouseManagementService/entity/Warehouse.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/Warehouse.java
@@ -1,17 +1,25 @@
 package ru.wms.WarehouseManagementService.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Warehouse {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 2, message = "Имя должно содержать не менее 2 символов")
     private String name;
 
     private String address;
@@ -22,52 +30,4 @@ public class Warehouse {
 
     @Column(name = "creation_date")
     private int creationDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public String getContactInformation() {
-        return contactInformation;
-    }
-
-    public void setContactInformation(String contactInformation) {
-        this.contactInformation = contactInformation;
-    }
-
-    public int getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(int creationDate) {
-        this.creationDate = creationDate;
-    }
 }
