953b259d8fda4ecc5f764fb0de76717cd261c37c
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-09 01:08:30 +0500

    added controllers & repo

--- src/main/java/ru/wms/WarehouseManagementService/entity/Product.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/Product.java
@@ -26,63 +26,6 @@ public class Product {
     private String category;
     private BigDecimal price;
     private int quantity;
-    @Column(name = "creation_date")
+    @Column(name = "creation_date", columnDefinition = "DATE")
     private LocalDate creationDate;
-
-    // getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 }
--- src/main/java/ru/wms/WarehouseManagementService/entity/Warehouse.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/Warehouse.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -28,55 +30,6 @@ public class Warehouse {
 
     private String contactInformation;
 
-    @Column(name = "creation_date")
-    private int creationDate;
-
-    // getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public String getContactInformation() {
-        return contactInformation;
-    }
-
-    public void setContactInformation(String contactInformation) {
-        this.contactInformation = contactInformation;
-    }
-
-    public int getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(int creationDate) {
-        this.creationDate = creationDate;
-    }
+    @Column(name = "creation_date", columnDefinition = "DATE")
+    private LocalDate creationDate;
 }
