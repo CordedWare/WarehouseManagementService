02921f8f8b2d2a309e27e8b4db87637018169bd1
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-08 23:24:27 +0500

    added entity

--- src/main/java/ru/wms/WarehouseManagementService/entity/Product.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/Product.java
@@ -1,4 +1,82 @@
 package ru.wms.WarehouseManagementService.entity;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @NotNull
+    @Size(min = 2, message = "Имя должно содержать не менее 2 символов")
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
