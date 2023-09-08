8360efe774e18a9511c8a6370e04c30a99be97b8
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-09 00:08:31 +0500

    added entities (Order, Product, User) & ProductController

--- src/main/java/ru/wms/WarehouseManagementService/entity/User.java
+++ src/main/java/ru/wms/WarehouseManagementService/entity/User.java
@@ -1,4 +1,20 @@
 package ru.wms.WarehouseManagementService.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+    private boolean active;
+    @ElementCollection(targetClass = UserRole.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<UserRole> roles;
 }
