b393bdc16a8e6d7b1c59c8e73692edb457445700
Samson-Miodek <video.kr4.123@gmail.com>
2023-09-12 20:50:23 +0500

    temp

--- src/main/java/ru/wms/WarehouseManagementService/controller/MainController.java
+++ src/main/java/ru/wms/WarehouseManagementService/controller/MainController.java
@@ -1,8 +1,18 @@
 package ru.wms.WarehouseManagementService.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.wms.WarehouseManagementService.security.UserPrincipal;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String index(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        var user = userPrincipal.getUser();
+        System.out.println(user);
+        return "index";
+    }
 
 }
