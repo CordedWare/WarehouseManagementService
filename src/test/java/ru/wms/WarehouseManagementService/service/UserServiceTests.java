af26e7bd3c0420c44ea7e96fdbbff63348bee9a4
Samson-Miodek <video.kr4.123@gmail.com>
2023-12-12 22:22:27 +0500

    upd: unit test

--- src/test/java/ru/wms/WarehouseManagementService/service/UserServiceTests.java
+++ src/test/java/ru/wms/WarehouseManagementService/service/UserServiceTests.java
@@ -4,15 +4,22 @@ import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.wms.WarehouseManagementService.dto.RegistrationForm;
+import ru.wms.WarehouseManagementService.entity.Customer;
 import ru.wms.WarehouseManagementService.entity.User;
 import ru.wms.WarehouseManagementService.repository.CustomerRepository;
 import ru.wms.WarehouseManagementService.repository.UserRepository;
+import ru.wms.WarehouseManagementService.security.Authority;
 
+import java.util.Collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -52,8 +59,23 @@ class UserServiceTests {
     @Test
     void registerUserCustomer() {
         var customer = userService.registerUserCustomer(registrationForm);
+
         assertNotNull(customer);
-        assertTrue(passwordEncoder.matches(registrationForm.getPassword(),customer.getPassword()));
+        assertTrue(passwordEncoder.matches(registrationForm.getPassword(), customer.getPassword()));
         assertTrue(CoreMatchers.is(customer.getEmail()).matches(registrationForm.getEmail()));
+        assertTrue(CoreMatchers.is(customer.getFirstname()).matches(registrationForm.getFirstname()));
+        assertTrue(CoreMatchers.is(customer.getLastname()).matches(registrationForm.getLastname()));
+        assertTrue(CoreMatchers.is(customer.getPatronymic()).matches(registrationForm.getPatronymic()));
+        assertTrue(CoreMatchers.is(customer.getTelephone()).matches(registrationForm.getTelephone()));
+        assertTrue(CoreMatchers.is(customer.getNameOrg()).matches(registrationForm.getNameOrg()));
+        assertTrue(CoreMatchers.is(customer.getAddressOrg()).matches(registrationForm.getAddressOrg()));
+        assertTrue(CoreMatchers.is(customer.getContactInfoOrg()).matches(registrationForm.getContactInfoOrg()));
+        assertTrue(CoreMatchers.is(customer.isActive()).matches(false));
+        assertEquals(customer.getAuthorities(), Collections.singleton(Authority.ROLE_CUSTOMER));
+
+        assertNotNull(customer.getActivationCode());
+
+        Mockito.verify(customerRepository, Mockito.times(1))
+                .save(Mockito.any(Customer.class));
     }
 }
