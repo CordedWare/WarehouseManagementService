00c1f68143e1d5b0b130429994bff107a65f1c2e
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-12-17 20:26:36 +0500

    Возврат сортировки продукта

--- src/main/java/ru/wms/WarehouseManagementService/controller/product/MainProductController.java
+++ src/main/java/ru/wms/WarehouseManagementService/controller/product/MainProductController.java
@@ -1,18 +1,24 @@
 package ru.wms.WarehouseManagementService.controller.product;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.wms.WarehouseManagementService.entity.Product;
 import ru.wms.WarehouseManagementService.entity.Warehouse;
-import ru.wms.WarehouseManagementService.repository.ProductRepository;
 import ru.wms.WarehouseManagementService.security.UserPrincipal;
 import ru.wms.WarehouseManagementService.service.ProductService;
 import ru.wms.WarehouseManagementService.service.WarehouseService;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/products")
@@ -26,25 +32,44 @@ public class MainProductController {
     private ProductService productService;
 
     @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
     private WarehouseService warehouseService;
 
-    @GetMapping
-    public String products(
-            @AuthenticationPrincipal UserPrincipal userPrincipal,
-            Model model
+    @GetMapping("/add")
+    public String add(@RequestParam(required = true) Long warehouseId,
+                      Model model,
+                      @AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
-        var user = userPrincipal.getUser();
-        Iterable<Product> productList = productService.getAllMyProducts(user);
-        Iterable<Warehouse> warehouseList = warehouseService.getAllMyWarehouses(user);
+
+        var user = userPrincipal.getUser().getCompany();
+        Optional<Iterable<Warehouse>> warehouseList = warehouseService.findAllById(warehouseId);
 
         model.addAttribute("warehouses", warehouseList);
-        model.addAttribute("products", productList);
         model.addAttribute("product", new Product());
+        return "product/add";
+    }
 
-        return "/product/products";
+
+    @PostMapping("/add")
+    public String add(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @ModelAttribute("product")
+            @Valid Product product,
+            @Valid Long warehouse,
+            BindingResult bindingResult
+    ) {
+        Optional.of(bindingResult)
+                .filter(BindingResult::hasErrors)
+                .ifPresent(errors -> {
+                    throw new IllegalArgumentException("Неверные данные о товаре");
+                });
+
+        productService.createProduct(
+                product,
+                warehouse,
+                userPrincipal.getUser()
+        );
+
+        return "redirect:/warehouses";
     }
 
     @GetMapping("/{id}")
@@ -53,53 +78,72 @@ public class MainProductController {
                     name = "filter",
                     required = false,
                     defaultValue = "")
-            Optional<String> filter, Model model
-    ) {
-        Iterable<Product> productList;
-        if (filter.isPresent() && !filter.isEmpty()) {
-            productList = productRepository.findByNameContaining(filter.get());
-        } else {
-            productList = productRepository.findAll();
-        }
-        model.addAttribute("products", productList);
-        model.addAttribute("filter", filter);
-        model.addAttribute("product", new Product());
-
-        return "products";
-    }
-
-    @PostMapping("/{id}/delete")
-    public String deleteProduct(@PathVariable("id") Long id) {
-        productService.deleteProductById(id);
-
-        return "redirect:/products";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String showEditProductForm(
-            @PathVariable("id") Long id,
+            Optional<String> nameFilterOpt,
             Model model
     ) {
-        Optional<Product> productOptional = Optional.ofNullable(productService.getProductById(id)); //TODO порефакторить с Optional более лаконично
+        Optional<Iterable<Product>> productList = Optional.of(
+                nameFilterOpt
+                        .filter(filter -> !filter.isEmpty())
+                        .flatMap(name -> productService.findByNameContaining(name))
+                        .orElseGet(() -> productService.getAllProducts().orElse(new ArrayList<>()))
+        );
 
-        if (productOptional.isPresent()) {
-            model.addAttribute("product", productOptional.get());
+        model.addAttribute("products", productList);
+        model.addAttribute("filter", nameFilterOpt);
+        model.addAttribute("product", new Product());
 
-            return "edit-product";
-        } else {
-            throw new RuntimeException("Товар не найден по id: " + id);
-        }
+        return "product/products";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, HttpServletRequest request) {
+        productService.deleteProductById(id);
+
+        var referer = request.getHeader("Referer");
+
+        return "redirect:" + referer;
     }
 
     @PostMapping("/{id}/edit")
     public String saveEditedProduct(
-            @PathVariable("id") Long id,
-            @ModelAttribute("product")
-            Product product
+            @PathVariable Long id,
+            @ModelAttribute Product editedProduct,
+            HttpServletRequest request
     ) {
-        productService.updateProduct(id, product);
+        productService.updateProduct(id, editedProduct);
+        var referer = request.getHeader("Referer");
 
-        return "redirect:/products";
+        return "redirect:" + referer;
+    }
+
+    @PostMapping("/filter")
+    public String filterProducts(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestParam(defaultValue = "asc") String sort,
+            @RequestParam(defaultValue = "") String filter,
+            Model model
+    ) {
+        Optional<Iterable<Product>> productList = productService.getAllProducts();
+
+        productList.ifPresent( products -> {
+            if (sort.equals("asc"))
+                products = StreamSupport.stream(products.spliterator(), false)
+                        .sorted(Comparator.comparing(product ->
+                                product.getPrice()))
+                        .collect(Collectors.toList());
+
+            else if (sort.equals("desc"))
+                products = StreamSupport.stream(products.spliterator(), false)
+                        .sorted(Comparator.comparing( (Product product) ->
+                                product.getPrice()).reversed())
+                        .collect(Collectors.toList());
+
+            model.addAttribute("products", products);
+        });
+
+        model.addAttribute("product", new Product());
+
+        return "product/products";
     }
 
 }
